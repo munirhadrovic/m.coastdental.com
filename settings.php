5a9e6ffb8a6dd20295ed5df1506337034783afe3
anes89cok <anes89cok@gmail.com>
2015-04-24 11:45:17 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,36 @@
+<?php
+$adresa[0]="761st Tank Battalion Bldg 330 Fort Hood,TX 76544"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Fort Hood";
+$telephone[0]=2542852014;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="20 Baker Rd Newnan,GA 30265"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="Bonita Springs, FL 34135";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="Newnan";
+$telephone[1]=6788540169;// Broj koji se poziva na dugme CALL US
+
+$adresa[2]="3308 Georgia 5 Suite E Douglasville,GA 30135"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[2]="Douglasville";
+$telephone[2]=7709470006;// Broj koji se poziva na dugme CALL US
+
+$adresa[3]="3509 Baker Rd Suite 401 Acworth,GA 30101"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[3]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[3]="Acworth";
+$telephone[3]=7709178943;// Broj koji se poziva na dugme CALL US
+
+$adresa[4]="4875 Floyd Rd SW Suite 113 Mableton,GA 30126"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[4]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[4]="Mableton";
+$telephone[4]=7707320900;// Broj koji se poziva na dugme CALL US
+
+
+$companynumber=5;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip","flow", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
